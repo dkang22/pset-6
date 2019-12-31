a93783d5f75027e8ed844740f8a36ba180515c17
dkang22 <daniellekang24@gmail.com>
2019-12-31 15:25:51 -0500

    made box around list elements

--- js/app.js
+++ js/app.js
@@ -23,191 +23,63 @@ function newTask(task) {
 */
 const list = document.getElementById("tasks-list");
 const input = document.getElementById("input");
-const PRIORITY_BUTTON = "fa fa-exclamation-circle";
+const PRIORITY_BUTTON = "fa fa-exclamation-circle priority";
 const CHECKMARK_BUTTON = "fa fa-check complete";
-const DELETE_BUTTON = "fa fa-trash-o delete";
+const DELETE_BUTTON = "fa fa-trash-o remove";
 const STRIKE_THROUGH = "lineThrough";
+let taskArray = [];
+let id = 0;
 
-function newTask(input) {
-    var item = `
-                      <li class="item">
-                        <span><button class="fa fa-exclamation-circle" id="0"></button></i>
-                        <span class="to-do-text"> ${input} </span>
-                        <span><button class="fa fa-check complete" id="0"></button></i>
-                        <span><button class="fa fa-trash-o delete" id="0"></button></i>
-                      </li>
-                  `;
-    list.insertAdjacentHTML("beforeend", item);
+function newTask(task, index, priority, complete, remove) {
+    task = input.value;
+    index = id;
+    priority = false;
+    complete = false;
+    remove = false;
 
-}
-
-
-//onclick="newTask()" href="javascript:void(0);"
-
-/* maybe this will work
-let list = [];
-
-//CREATING A NEW TASK
-function newTask() {
-//text
-    var listElement = document.createElement("li");
-    var inputedTask = document.getElementById("input").value;
-    var text = document.createTextNode(inputedTask);
-    listElement.appendChild(text);
-    if (inputedTask === "") {
-        alert("Please enter a task!")
+    if (input.value == "") {
+        alert("Please enter a task to-do!");
     } else {
-        document.getElementById("tasks-list").appendChild(listElement);
+        taskArray.push({
+            name: task,
+            index: index,
+            priority: false,
+            complete: false,
+            remove: false
+        });
+        id++;
+        var item = `
+                          <li class="item">
+                            <span><button class="fa fa-exclamation-circle priority" id="priority-button"></button></i>
+                            <span class="to-do-text" id="task-text">  ${task}  </span>
+                            <span><button class="fa fa-check complete" id="complete-button"></button></i>
+                            <span><button class="fa fa-trash-o remove" id="remove-button"></button></i>
+                          </li>
+                    `;
+        list.insertAdjacentHTML("beforeend", item);
+        input.value = "";
     }
-    document.getElementById("input").value = "";
-//priority button
-    var span = document.createElement("SPAN");
-    var priorityButton = document.createTextNode("fa fa-exclamation-circle");
-
 
 }
 
-/*
-<li class="item">
-  <i class="fa fa-exclamation-circle"></i>
-  <p class="to-do-text">Complete p-set6</p>
-  <i class="fa fa-check complete"></i>
-  <i class="fa fa-trash-o delete"></i>
-</li>
-
-/*
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle-thin";
-const LINE_THROUGH = "lineThrough";
-
-document.addEventListener("keyup", function(event)){
-    if(event.keyCode ==13){
-        const addTask = input.value;
-        if (addTask){
-            addTod(toDo, id, false, false);
-            LIST.push(
-              {
-                  name: Task,
-                  id: id,
-                  done: false,
-                  trash: false
-              }
-            );
-            input.value = "";
+document.addEventListener("keyup", function(parameter){
+    if(event.keyCode == 13) {
+        if(input.value) {
+            newTask(input.value);
+        } else {
+            alert("Please enter a task to-do!");
         }
     }
+});
+
+function prioritizeTask(element) {
+
 }
 
-function addTask(task, id, done, trash) {
-    const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINE_THROUGH : "";
+function completeTask(element) {
 
-    if(trash){
-        return;
-    }
-    const text = `<li class="item">
-                      <i class="fa fa-exclamation-circle"></i>
-                      <p class="text ${LINE}">  ${task}  </p>
-                      <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
-                      <i class="fa fa-trash-o delete" job="delete" id="${id}"></i>
-                  </li>`
-
-    const position = "beforeend";
-
-    list.insertAdjacentHTML(position, text);
 }
 
-function check
+function removeTask(element) {
 
-
-/*
-let tasker = {
-    selectElements: function() {
-        this.taskInput = documnent.getElementById("input-task");
-        this.taskList = document.getElementById("tasks");
-        this.taskListChildren = this.taskList.children;
-        this.addButton = document.getElementbyId("add-task-button");
-        this.errorMessage = document.getElementById("error");
-    },
-    buildTask: function() {
-        let taskListItem;
-        let taskCheckbox;
-        let taskValue;
-        let taskBUtton;
-        let taskTrash;
-
-        taskListItem = document.createElement("li");
-        taskListItem.setAttribute("class", "task");
-
-        taskCheckbox = document.createElement("input");
-        taskCheckbox.setAttribute("type", "checkbox");
-
-        taskValue = document.createTextNode(this.taskInput.value);
-
-        taskButton = document.createElement("button");
-
-        taskTrash  document.createElement("i");
-        taskTrash.setAttribute("class", "fa fa-trash");
-
-        taskButton.appendChild(taskTrash);
-        taskListItem.appendChild(taskCheckbox);
-        taskListItem.appendChild(taskButton);
-
-        this.taskList.appendChild(taskListItem);
-    },
-    addTask: function() {
-        let taskValue = this.taskInput.value;
-        this.buildTask();
-        this.taskInput.value = "";
-        this.scanTaskList();
-    },
-    enterKey: function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            this.addTask();
-        }
-    },
-    bindEvents: function() {
-        this.addButton.onclick = this.addTask.bind(this);
-        this.taskInput.onkeypress = this.enterKey.bind(this);
-    }
-};
-
-
-// idea for new tasks
-<form action="">
-    <input
-      type="text"
-      class="new list"
-      placeholder="enter a new task"
-    />
-    <button class="button-task"
-    aria-label="create new task">+</button>
-</form>
-
-//idea for existing tasks
-<button class="priority">Prioritize</button>
-<label for="task-1">
-    work on pset-6
-</label>
-<button class="completed-task">Checkbox</button>
-<button class="deleted-task">Trash Bin</button>
-</div>
-<div class="tasks">
-<button class="priority">Prioritize</button>
-<label for="task-2">
-    english research project
-</label>
-<button class="completed-task">Checkbox</button>
-<button class="deleted-task">Trash Bin</button>
-</div>
-
-*/
-
-/*
-                    <li class="item">
-                      <i class="fa fa-exclamation-circle"></i>
-                      <p class="to-do-text">Complete p-set6</p>
-                      <i class="fa fa-check complete"></i>
-                      <i class="fa fa-trash-o delete"></i>
-                    </li>
-*/
+}
